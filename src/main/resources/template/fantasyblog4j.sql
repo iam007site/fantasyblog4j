b93f0d0b6e3b97fdde346aaebd235939b8a11912
huangshanqi <huangshanqi@neotel.co,.cn>
2016-04-12 18:10:10 +0800

    update

--- src/main/resources/template/fantasyblog4j.sql
+++ src/main/resources/template/fantasyblog4j.sql
@@ -1,6 +1,6 @@
-﻿# Host: localhost  (Version: 5.5.27)
-# Date: 2016-04-10 22:26:59
-# Generator: MySQL-Front 5.3  (Build 4.198)
+﻿# Host: 127.0.0.1  (Version: 5.5.43)
+# Date: 2016-04-12 17:22:28
+# Generator: MySQL-Front 5.3  (Build 4.233)
 
 /*!40101 SET NAMES utf8 */;
 
@@ -18,13 +18,11 @@ CREATE TABLE `p_post` (
   `ctime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `mtime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8 COMMENT='post table';
+) ENGINE=InnoDB AUTO_INCREMENT=100008 DEFAULT CHARSET=utf8 COMMENT='post table';
 
 #
 # Data for table "p_post"
 #
-
-INSERT INTO `p_post` VALUES (100000,100000,'aaa','bb',0,'2016-04-10 15:33:26','2016-04-10 15:33:26'),(100001,100000,'aa','bbb',0,'2016-04-10 15:37:13','2016-04-10 15:37:13'),(100002,100000,'我爱老婆','我和老婆',0,'2016-04-10 15:49:35','2016-04-10 15:49:35'),(100003,100000,'lkaflafajlf','Bootstrap',0,'2016-04-10 21:28:52','2016-04-10 21:28:52');
 
 #
 # Structure for table "p_post_detail"
@@ -38,13 +36,11 @@ CREATE TABLE `p_post_detail` (
   `ctime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `mtime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=100008 DEFAULT CHARSET=utf8;
 
 #
 # Data for table "p_post_detail"
 #
-
-INSERT INTO `p_post_detail` VALUES (100000,100000,'<pre>Please write your blog here...fffffffff</pre><blockquote><p>afafafasf</p><p>afasfasf</p></blockquote><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p>','2016-04-10 15:33:26','2016-04-10 15:33:26'),(100001,100001,'<p>Please write your blog here...ffffaaaaaaaaaa</p><h1>dddd</h1><pre>afasfasfasfa<br>public static void main(){<br>&nbsp; &nbsp; System.out.println(\"Hello Word\");<br>}</pre>','2016-04-10 15:37:13','2016-04-10 15:37:13'),(100002,100002,'<p>Please write your blog here..我爱老婆</p>','2016-04-10 15:49:35','2016-04-10 15:49:35'),(100003,100003,'<p>fafsfasfasfcaffs</p><p>as<br>fas</p><p>faf</p><p>af</p><p>af</p><p>af</p><p>aF</p><p>af</p><p>aF</p><p>aF</p><p>aF</p><p>asF</p><p>aF</p><p>ff</p><p>f</p><p>F</p>','2016-04-10 21:28:52','2016-04-10 21:28:52');
 
 #
 # Structure for table "p_post_tag"
@@ -61,31 +57,15 @@ CREATE TABLE `p_post_tag` (
   `mtime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
   KEY `user_post_tag_index` (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8 COMMENT='post tag table';
+) ENGINE=InnoDB AUTO_INCREMENT=100021 DEFAULT CHARSET=utf8 COMMENT='post tag table';
 
 #
 # Data for table "p_post_tag"
 #
 
-INSERT INTO `p_post_tag` VALUES (100000,100000,100000,'ccc',0,'2016-04-10 15:33:26','2016-04-10 15:33:26'),(100001,100000,100000,'ddd',0,'2016-04-10 15:33:26','2016-04-10 15:33:26'),(100002,100000,100000,'eee',0,'2016-04-10 15:33:26','2016-04-10 15:33:26'),(100003,100000,100001,'cc',0,'2016-04-10 15:37:13','2016-04-10 15:37:13'),(100004,100000,100001,'dd',0,'2016-04-10 15:37:13','2016-04-10 15:37:13'),(100005,100000,100001,'eee',0,'2016-04-10 15:37:13','2016-04-10 15:37:13'),(100006,100000,100002,'老婆大人',0,'2016-04-10 15:49:35','2016-04-10 15:49:35'),(100007,100000,100003,'afasfasf',0,'2016-04-10 21:28:52','2016-04-10 21:28:52'),(100008,100000,100003,'afafa',0,'2016-04-10 21:28:52','2016-04-10 21:28:52'),(100009,100000,100003,'fafaf',0,'2016-04-10 21:28:52','2016-04-10 21:28:52');
-
 #
 # Structure for table "test"
 #
-
-DROP TABLE IF EXISTS `test`;
-CREATE TABLE `test` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL DEFAULT '',
-  `password` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=100005 DEFAULT CHARSET=utf8 COMMENT='for test';
-
-#
-# Data for table "test"
-#
-
-INSERT INTO `test` VALUES (100000,'hsq','1312'),(100001,'hsq','1312'),(100002,'hsq','1312'),(100003,'hsq','1312'),(100004,'hsq','1312');
 
 #
 # Structure for table "u_user"
@@ -105,10 +85,10 @@ CREATE TABLE `u_user` (
   `mtime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `last_login_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=100001 DEFAULT CHARSET=utf8 COMMENT='user table';
+) ENGINE=InnoDB AUTO_INCREMENT=100002 DEFAULT CHARSET=utf8 COMMENT='user table';
 
 #
 # Data for table "u_user"
 #
 
-INSERT INTO `u_user` VALUES (100000,'test','4bb048b9b6ca1e7065bf10f570b64b4741ba8940','ea32e267bc689739','test@test','test',1,'','2016-04-07 22:07:04','2016-04-07 22:07:04','2016-04-07 22:07:04');
+INSERT INTO `u_user` VALUES (100001,'evilcoder','515a5e5ecc9012533810c50b8400f7066616fbf2','6ca1b4997efa157b','hsqmobile@gmail.com','evilcoder',1,'','2016-04-12 17:21:40','2016-04-12 17:21:40','2016-04-12 17:21:40');
